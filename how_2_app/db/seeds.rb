d01f5c2e83c9db433157f9c5b880e9b77ab5b763
DaveMitten <davidmitten88@gmail.com>
2017-05-30 15:50:51 +0100

    fiddled with schemas and models. Handing duties to harry

--- how_2_app/db/seeds.rb
+++ how_2_app/db/seeds.rb
@@ -1,4 +1,4 @@
 u1 = User.create!( username: "bob", first_name: "Harry", last_name: "Stirling", email: "harry@harry.com", password: "password", password_confirmation: "password", image: "http://fillmurray.com/100/100")
 
-v1 = u1.videos.create!( title: "How to make a video game.", youtube_id: "YIcOjY94QlI", description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ", meta_tags: "games, other shit"
+v1 = u1.videos.create!(title: "How to make a video game.", youtube_id: "YIcOjY94QlI", description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ", meta_tags: "games, other shit"
 )
