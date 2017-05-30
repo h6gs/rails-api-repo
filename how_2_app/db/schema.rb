0f0e8720f8f71c03b23bcf6f18f5305fabd43328
DaveMitten <davidmitten88@gmail.com>
2017-05-30 15:21:08 +0100

    fixed error of no username with seeds file. Now seeded

--- how_2_app/db/schema.rb
+++ how_2_app/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170530140128) do
+ActiveRecord::Schema.define(version: 20170530142015) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 20170530140128) do
     t.string "image"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "username"
   end
 
   create_table "videos", force: :cascade do |t|
